dacfc125945b1fa11872efbf95bb9c40482797eb
phmLabs <nils.langner@phmlabs.com>
2017-08-17 07:08:40 +0200

    better regex

--- src/Uri.php
+++ src/Uri.php
@@ -716,9 +716,11 @@ class Uri implements CookieAware, UriInterface
     {
         $count = 1;
         $cleanPath = $uri->getPath();
+
         while ($count != 0) {
-            $cleanPath = preg_replace('~[^\/]+\/\.\.\/~', '', $cleanPath, -1, $count);
+            $cleanPath = preg_replace('~[^\/\.]+\/\.\.\/~', '', $cleanPath, -1, $count);
         }
+
         return $uri->withPath($cleanPath);
     }
 }
